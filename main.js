ebc17225557ec3d33913c0010089b35e13f76aaf
Xd08 <76774039+Xd08@users.noreply.github.com>
2021-01-09 12:41:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+
+function modelLoaded() {
+  console.log("mobile net model has been Loaded")
+}
+
+function gotResult(error, result) {
+  if(error) {
+    console.log(error)
+  }
+  else {
+    console.log(result)
+    document.getElementById("result_object_name").innerHTML=result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=result[0].confidence.toFixed(1);
+  }
+}
